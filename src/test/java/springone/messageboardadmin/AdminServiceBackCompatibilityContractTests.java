949cc30631efe9b5b252fae1ac4ebff28d476be2
Cora Iberkleid <ciberkleid@gmail.com>
2021-08-25 20:47:30 -0400

    remove unused import

--- src/test/java/springone/messageboardadmin/AdminServiceBackCompatibilityContractTests.java
+++ src/test/java/springone/messageboardadmin/AdminServiceBackCompatibilityContractTests.java
@@ -1,6 +1,5 @@
 package springone.messageboardadmin;
 
-import org.assertj.core.api.BDDAssertions;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
